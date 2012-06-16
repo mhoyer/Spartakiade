27d82fb093ed843002e732a74bf077dac931185e
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-06-16 12:48:58 +0200

    implemented glue between producer -> observer -> consumer

--- Rx/FizzBuzz/FizzBuzz/Program.cs
+++ Rx/FizzBuzz/FizzBuzz/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace FizzBuzz
+﻿using System;
+
+namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var producer = new NumberProducer();
+            var observer = new NumberObservable(producer);
+
+            var consumer = new ConsoleOutputObserver();
+
+            observer.Subscribe(consumer);
+            producer.Produce();
+            
+            Console.ReadKey();
         }
     }
 }
