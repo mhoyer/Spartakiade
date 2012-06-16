01bff0e431e7f0bd9d7eebdc7b3ddd0797aafc89
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-06-16 12:45:30 +0200

    added NumberObservable

---
+++ Rx/FizzBuzz/FizzBuzz/NumberObservable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+
+namespace FizzBuzz
+{
+    public class NumberObservable : IObservable<int>
+    {
+        private IList<IObserver<int>> _obsersers;
+
+        public NumberObservable(NumberProducer producer)
+        {
+            _obsersers = new List<IObserver<int>>();
+            producer.OnNumber += 
+                i => _obsersers.ToList().ForEach(o => o.OnNext(i));
+        }
+
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            _obsersers.Add(observer);
+
+            return Disposable.Empty;
+        }
+    }
+}
