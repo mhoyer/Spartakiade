1943bd750cdd5887a632ba7c625394ef4dc074fc
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-06-16 12:46:34 +0200

    added ConsoleOutputObserver

---
+++ Rx/FizzBuzz/FizzBuzz/ConsoleOutputObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class ConsoleOutputObserver : IObserver<int>
+    {
+        public void OnNext(int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("error: {0}", error);
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("done");
+        }
+    }
+}
